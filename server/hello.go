21e2f3a98f0de1a1a056871db1b38360b678c415
Hiroyuki Ota <hiroyuki.ot@gmail.com>
2015-07-06 20:32:01 +0900

    .

--- server/hello.go
+++ server/hello.go
@@ -3,90 +3,79 @@ package guestbook
 import (
 	"html/template"
 	"net/http"
-	"time"
 
 	"appengine"
 	"appengine/datastore"
 	"appengine/user"
-
-	_ "api"
 )
-
-type Greeting struct {
-	Author  string
-	Content string
-	Date    time.Time
-}
 
 func init() {
 	http.HandleFunc("/", root)
-	http.HandleFunc("/sign", sign)
-}
-
-// guestbookKey returns the key used for all guestbook entries.
-func guestbookKey(c appengine.Context) *datastore.Key {
-	// The string "default_guestbook" here could be varied to have multiple guestbooks.
-	return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
+	http.HandleFunc("/register", register)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	// Ancestor queries, as shown here, are strongly consistent with the High
-	// Replication Datastore. Queries that span entity groups are eventually
-	// consistent. If we omitted the .Ancestor from this query there would be
-	// a slight chance that Greeting that had just been written would not
-	// show up in a query.
-	q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
-	greetings := make([]Greeting, 0, 10)
-	if _, err := q.GetAll(c, &greetings); err != nil {
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	q := datastore.NewQuery("Device").Filter("UserId =", u.ID).Order("DeviceName")
+
+	devices := make([]Device, 10)
+	if _, err := q.GetAll(c, &devices); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	if err := guestbookTemplate.Execute(w, greetings); err != nil {
+	data := map[string]interface{}{
+		"User":    u,
+		"Devices": devices,
+	}
+
+	if err := registerTemplate.Execute(w, &data); err != nil {
+		//if err := registerTemplate.Execute(w, data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-var guestbookTemplate = template.Must(template.New("book").Parse(`
-<html>
-  <head>
-    <title>Go Guestbook</title>
-  </head>
-  <body>
-    {{range .}}
-      {{with .Author}}
-        <p><b>{{.}}</b> wrote:</p>
-      {{else}}
-        <p>An anonymous person wrote:</p>
-      {{end}}
-      <pre>{{.Content}}</pre>
-    {{end}}
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-`))
+var registerTemplate = template.Must(template.ParseFiles("register.html"))
 
-func sign(w http.ResponseWriter, r *http.Request) {
+func register(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	g := Greeting{
-		Content: r.FormValue("content"),
-		Date:    time.Now(),
+	u := user.Current(c)
+	if u == nil {
+		http.Redirect(w, r, "/", http.StatusFound)
 	}
-	if u := user.Current(c); u != nil {
-		g.Author = u.String()
+
+	device := r.FormValue("device_id")
+	deviceName := r.FormValue("device_name")
+	// Default threshold is 15
+	threshold := int32(15)
+
+	strKey := u.String() + ":" + device
+	g := Device{
+		UserId:         u.ID,
+		DeviceId:       device,
+		DeviceName:     deviceName,
+		AlertThreshold: threshold,
 	}
-	// We set the same parent key on every Greeting entity to ensure each Greeting
-	// is in the same entity group. Queries across the single entity group
-	// will be consistent. However, the write rate to a single entity group
-	// should be limited to ~1/second.
-	key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
+
+	key := datastore.NewKey(c, "Device", strKey, 0, nil)
 	_, err := datastore.Put(c, key, &g)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func registerDevice(u *user.User, c appengine.Context, w http.ResponseWriter, r *http.Request) {
 }
