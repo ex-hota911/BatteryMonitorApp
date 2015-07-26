e53e94873161fe710a4fd2ff60cf25ded1dd1ed1
Hiroyuki Ota <hiroyuki.ot@gmail.com>
2015-07-26 23:48:17 +0900

    Update project

--- android/app/src/main/java/jp/hota/batterymonitor/MainActivity.java
+++ android/app/src/main/java/jp/hota/batterymonitor/MainActivity.java
@@ -8,10 +8,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.appspot.icumn7abiu.battery.Battery;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.json.gson.GsonFactory;
+
 
 public class MainActivity extends ActionBarActivity {
 
     static TextView textView;
+    private Battery service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,9 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.text);
         startService(new Intent(this, BatteryLogger.class));
+        Battery.Builder builder;
+        builder = new Battery.Builder(
+                AndroidHttp.newCompatibleTransport(), new GsonFactory(), null);
+        service = builder.build();
     }
 }
