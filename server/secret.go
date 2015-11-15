1ac3360ed26998962de5f68064fe98c84ab0ea80
Hiroyuki Ota <hiroyuki.ot@gmail.com>
2015-11-15 16:19:52 +0900

    .

--- server/secret.go
+++ server/secret.go
@@ -3,5 +3,5 @@ package server
 const (
 	webClientId            = "546634630324-mkannoor781g7scn86vodbhol9qss1ev.apps.googleusercontent.com"
 	androidReleaseClientId = "546634630324-o1b8i5jp5985tha39ob06vhhokmi04o3.apps.googleusercontent.com"
-	androidDebugClientId   = "546634630324-lhicestiq2l8osfobdhehi9iprgu9c3n.apps.googleusercontent.com	"
+	androidDebugClientId   = "546634630324-lhicestiq2l8osfobdhehi9iprgu9c3n.apps.googleusercontent.com"
 )
