485f7d5c0798e384f64dc1f160acc85eeae7efee
Hiroyuki Ota <hiroyuki.ot@gmail.com>
2016-01-17 19:21:54 +0900

    Chrome: Update battery status.

--- chrome/background.js
+++ chrome/background.js
@@ -1,61 +1,50 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-var ROOT = 'https://icumn7abiu.appspot.com/_ah/api'
+//var ROOT = 'https://icumn7abiu.appspot.com'
+var ROOT = 'localhost:8080'
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  navigator.getBattery()
-	.then(function(battery) {
-	  if (battery) {
-		console.log(battery.level * 100);
-		return battery.level * 100;
-	  }
-	});
-
-  gapi.client.load('battery', 'v1', function() {
-	console.log('ok');
-  }, ROOT);
-
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
 });
 
 chrome.alarms.onAlarm.addListener(function( alarm ) {
-  console.log("Got an alarm!", alarm);
 });
 
 chrome.runtime.onInstalled.addListener(function(details){
   chrome.alarms.create("checkBattery", {delayInMinutes: 1, periodInMinutes: 1});
 });
 
-function callback2(jsonResp, rawResp) {
-  console.log("callback2");
-  console.log(jsonResp);
-  console.log(rawResp);
+/**
+ * Updates the battery status.
+ */
+function updateBatteryStatus(battery) {
+  console.log(battery);
+  var level = battery.level * 100;
+  console.log(level);
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+	if (xhr.readyState != 4) {
+	  return;
+	}
+	console.log(xhr.status);
+	console.log(xhr.responseText);
+  };
+
+  var formData = new FormData();
+  // TODO: Set device ID.
+  formData.append("device_id", 'chrome_extension');
+  formData.append("battery", level);
+
+  xhr.open("POST", ROOT + "/battery");
+  xhr.send(formData);
 }
 
-function callback() {
-  console.log("callback");
-  gapi.client.request({
-	root: ROOT,
-	path: '/battery/v1/battery.hello',
-	callback: callback2,
+// Register listener.
+navigator.getBattery().then(function(battery) {
+  console.log(battery);
+  battery.addEventListener('levelchange', function() {
+	updateBatteryStatus(battery);
   });
-}
-
-function gapiIsLoaded() {
-  var params = { 'immediate': false };
-  
-  gapi.auth.authorize(params, function(accessToken) {
-	console.log("auth:");
-	console.log(accessToken);
-    if (accessToken) {
-      callback();
-    }
+  battery.addEventListener('chargingchange', function() {
+	updateBatteryStatus(battery);
   });
-}
+});
