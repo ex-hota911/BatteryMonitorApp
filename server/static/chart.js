0db92fc51be4426cbf7a4a48c687e90f759f58f8
Hiroyuki Ota <hiroyuki.ot@gmail.com>
2016-02-06 01:29:34 +0900

    Format chart.js

--- server/static/chart.js
+++ server/static/chart.js
@@ -5,44 +5,44 @@ google.load('visualization', '1.0', {'packages':['corechart']});
 google.setOnLoadCallback(drawCharts);
 
 function drawCharts() {
-	for (var i = 0; i < battery.length; i++) {
-		// TODO(hota): How to pass battery?
-	    drawChart(battery[i], "chart_" + i);
-    }
+  for (var i = 0; i < battery.length; i++) {
+	// TODO(hota): How to pass battery?
+	drawChart(battery[i], "chart_" + i);
+  }
 }
 
 function drawChart(d, id) {
-	var data = new google.visualization.DataTable();
-    data.addColumn('datetime', 'Time');
-    data.addColumn('number', 'Charging');
-    data.addColumn('number', 'On Battery');
+  var data = new google.visualization.DataTable();
+  data.addColumn('datetime', 'Time');
+  data.addColumn('number', 'Charging');
+  data.addColumn('number', 'On Battery');
 
-    for(var i = 0; i < d.length; i++) {
-	  if (i > 0 && d[i-1].charging != d[i].charging) {
-		// The state is changed. Adding data points for both in order to connect the lines
-		data.addRow([new Date(d[i].time), d[i].battery, d[i].battery]);
-	  } else if (d[i].charging) {
-		data.addRow([new Date(d[i].time), d[i].battery, null]);
-	  } else {
-		data.addRow([new Date(d[i].time), null, d[i].battery]);
+  for(var i = 0; i < d.length; i++) {
+	if (i > 0 && d[i-1].charging != d[i].charging) {
+	  // The state is changed. Adding data points for both in order to connect the lines
+	  data.addRow([new Date(d[i].time), d[i].battery, d[i].battery]);
+	} else if (d[i].charging) {
+	  data.addRow([new Date(d[i].time), d[i].battery, null]);
+	} else {
+	  data.addRow([new Date(d[i].time), null, d[i].battery]);
+	}
+  }
+
+  var options = {
+    title: 'Battery (%)',
+    legend: { position: 'bottom' },
+	series: {
+      0: { color: 'red' },
+      1: { color: 'blue' },
+	},
+	vAxis: {
+	  viewWindowMode: 'explicit',
+	  viewWindow: {
+		min: 0,
+		max: 100,
 	  }
-    }
-
-    var options = {
-      title: 'Battery (%)',
-      legend: { position: 'bottom' },
-	  series: {
-        0: { color: 'red' },
-        1: { color: 'blue' },
-	  },
-	  vAxis: {
-		viewWindowMode: 'explicit',
-		viewWindow: {
-		  min: 0,
-		  max: 100,
-		}
-	  },
-    };
-    var chart = new google.visualization.AreaChart(document.getElementById(id));
-    chart.draw(data, options);
+	},
+  };
+  var chart = new google.visualization.AreaChart(document.getElementById(id));
+  chart.draw(data, options);
 }
